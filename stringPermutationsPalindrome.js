9cf4dd716b49ca1877d47aa081dd7c3fd841e472
shankarv82 <shankarv82@gmail.com>
2021-05-07 12:41:36 +0530

    String Permutations Palindrome
    All permutations of a string
    ----------------------------
    permutations(abc) = a + permutations(bc)
    permutations(bc) = b + permutations(c) + c + permutations(b)
    permutations(b) = b
    permutations(c) = c
    
    Similarly: b + permutations(ac) and c + permutations(ab)

---
+++ stringPermutationsPalindrome.js
@@ -0,0 +1,28 @@
+function checkPalindrom(str) {
+    return str == str.split('').reverse().join('');
+}
+
+function getAllPermutations(string) {
+  const results = [];
+
+  if (string.length === 1) {
+    results.push(string);
+    return results;
+  }
+
+  for (let i = 0; i < string.length; i++) {
+    const firstChar = string[i];
+    const charsLeft = string.substring(0, i) + string.substring(i + 1);
+    const innerPermutations = getAllPermutations(charsLeft);
+    for (let j = 0; j < innerPermutations.length; j++) {
+      results.push(firstChar + innerPermutations[j]);
+    }
+  }
+  return results;
+}
+
+const permutaions = getAllPermutations('abc');
+
+for(let str  of permutaions) {
+  console.log(`Is ${str} Palindrome? `, checkPalindrom(str));
+}
