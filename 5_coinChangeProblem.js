6d552b78d4565bde67aee4d86e372e9edb60f711
shankarv82 <shankarv82@gmail.com>
2021-05-14 15:08:00 +0530

    Create 5_coinChangeProblem.js
    Coin change problem

---
+++ 5_coinChangeProblem.js
@@ -0,0 +1,24 @@
+const coinChangeProblem = (coins, amount) => {
+  const coinsLength = coins.length;
+  const ways = Array(amount+1).fill(0);
+
+  ways[0] = 1;
+  
+  //  0			1			2			3			4			5
+  // 	0			0			0			0			0			0
+  
+  
+  // 	1			1			1			1			1			1
+  //  						2			2			3			3
+  // 																4
+  
+	for (let i=0; i<coinsLength; i++) {
+  	for (let j=coins[i]; j<=amount; j++) {
+    	ways[j] = ways[j-coins[i]] + ways[j];
+    }
+  }
+  
+  return ways[amount];
+}
+
+console.log(coinChangeProblem([1,2,5], 5));
