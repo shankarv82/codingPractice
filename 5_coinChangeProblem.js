9909b9fb10ccbe4c95f1e8b2236a741ca26a70c1
shankarv82 <shankarv82@gmail.com>
2021-05-17 14:37:46 +0530

    Update 5_coinChangeProblem.js

--- 5_coinChangeProblem.js
+++ 5_coinChangeProblem.js
@@ -4,13 +4,13 @@ const coinChangeProblem = (coins, amount) => {
 
   ways[0] = 1;
   
-  //  0			1			2			3			4			5
-  // 	0			0			0			0			0			0
+  //  0  1  2  3  4  5
+  //  0  0  0  0  0  0
   
   
-  // 	1			1			1			1			1			1
-  //  						2			2			3			3
-  // 																4
+  //  1  1  1  1  1  1
+  //  2  2  3  3
+  //              4  4
   
 	for (let i=0; i<coinsLength; i++) {
   	for (let j=coins[i]; j<=amount; j++) {
