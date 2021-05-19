82fbcaa783c882a2d3714c3e7144272a3193ee74
shankarv82 <shankarv82@gmail.com>
2021-05-19 22:48:07 +0530

    Create 10_arrayFlatten.js

---
+++ 10_arrayFlatten.js
@@ -0,0 +1,24 @@
+// Flattening an array. Ex: Input : [[1, 2, 3], 4, [5, [6, 7]] Output: [1, 2, 3, 4, 5, 6, 7].
+
+const flatten = arr => {
+  return arr.reduce((acc, current) => {
+    return acc.concat(Array.isArray(current) ? flatten(current) : current);
+  }, []);
+}
+
+
+
+console.log(
+flatten(
+	[
+		[1, 2, 3], 
+    4, 
+    [5, 
+    	[6, 7]
+    ]
+  ]
+)
+); 
+
+// Output
+// [1, 2, 3, 4, 5, 6, 7]
