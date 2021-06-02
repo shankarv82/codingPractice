0834f620237b348af9ebf8c7a407ea7ab679eea6
shankarv82 <shankarv82@gmail.com>
2021-06-02 16:06:23 +0530

    Create 13_isCyclic.js

---
+++ 13_isCyclic.js
@@ -0,0 +1,21 @@
+const isCyclic = obj => {
+  const seenObjects = [];
+
+  const detect = obj => {
+    if (obj && typeof obj === 'object') {
+      if (seenObjects.indexOf(obj) !== -1) {
+        return true;
+      }
+      seenObjects.push(obj);
+      for (let key in obj) {
+        if (obj.hasOwnProperty(key) && detect(obj[key])) {
+          console.log(obj, 'cycle at ' + key);
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  return detect(obj);
+}
