524b9ea8246643501978a59fae9c12a5c8016627
shankarv82 <shankarv82@gmail.com>
2021-06-05 14:50:55 +0530

    Update 15_numberOfIcelands.js

--- 15_numberOfIcelands.js
+++ 15_numberOfIcelands.js
@@ -1,10 +1,8 @@
 // Javascript program to count islands in boolean 2D matrix
 
-// A function to check if a given cell (row, col) can
-// be included in DFS
-function isSafe(M, row, col, visited) {
-    // row number is in range, column number is in range
-    // and value is 1 and not yet visited
+// A function to check if a given cell (row, col) can be included in DFS
+const isSafeAndNotVisited = (M, row, col, visited) => {
+    // row number is in range, column number is in range and value is 1 and not yet visited
     return (
         row >= 0 &&
         row < ROW &&
@@ -15,11 +13,13 @@ function isSafe(M, row, col, visited) {
     );
 }
 
-// A utility function to do DFS for a 2D boolean matrix.
-// It only considers the 8 neighbors as adjacent vertices
-function DFS(M, row, col, visited) {
-    // These arrays are used to get row and column numbers
-    // of 8 neighbors of a given cell
+// A utility function to do DFS for a 2D boolean matrix. It only considers the 8 neighbors as adjacent vertices
+const DFS = (M, row, col, visited) => {
+    // These arrays are used to get row and column numbers of 8 neighbors of a given cell
+    // [-1, -1] [-1, 0] [-1, 1]
+    // [ 0, -1] [ 0, 0] [ 0, 1]
+    // [ 1, -1] [ 1, 0] [ 1, 1]
+    
     let rowNbr = [-1, -1, -1, 0, 0, 1, 1, 1];
     let colNbr = [-1, 0, 1, -1, 1, -1, 0, 1];
 
@@ -28,7 +28,7 @@ function DFS(M, row, col, visited) {
 
     // Recur for all connected neighbours
     for (let k = 0; k < 8; ++k) {
-        if (isSafe(M, row + rowNbr[k], col + colNbr[k], visited)) {
+        if (isSafeAndNotVisited(M, row + rowNbr[k], col + colNbr[k], visited)) {
             DFS(M, row + rowNbr[k], col + colNbr[k], visited);
         }
     }
@@ -36,26 +36,22 @@ function DFS(M, row, col, visited) {
 
 // The main function that returns count of islands in a given
 // boolean 2D matrix
-function countIslands(M) {
-    // Make a bool array to mark visited cells.
-    // Initially all cells are unvisited
-    let visited = new Array(ROW);
+const countIslands = M => {
+    // create an array marking all unvisited
+    const visited = [];
     for (let i = 0; i < ROW; i++) {
-        visited[i] = new Array(COL);
+        visited[i] = [];
         for (let j = 0; j < COL; j++) {
-            visited[i][j] = false;
+            visited[i].push(false);
         }
     }
 
-    // Initialize count as 0 and travese through the all cells
-    // of given matrix
+    // Initialize count as 0 and travese through the all cells of given matrix
     let count = 0;
-    for (let i = 0; i < ROW; ++i) {
-        for (let j = 0; j < COL; ++j) {
+    for (let i = 0; i < ROW; i++) {
+        for (let j = 0; j < COL; j++) {
             if (M[i][j] == 1 && !visited[i][j]) {
-                // value 1 is not
-                // visited yet, then new island found, Visit all
-                // cells in this island and increment island count
+                // value 1 is not visited yet, then new island found, Visit all cells in this island and increment island count
                 DFS(M, i, j, visited);
                 count++;
             }
