5c43469ea4bb0d8acb883fed3889350719b96456
shankarv82 <shankarv82@gmail.com>
2024-01-16 13:18:28 +0530

    Update 5_coinChangeProblem.js

--- 5_coinChangeProblem.js
+++ 5_coinChangeProblem.js
@@ -1,3 +1,20 @@
+// Solution 1
+const coinChange = (coins, amount) => {
+  const dp = Array(amount + 1).fill(Infinity); // This arr tells us how many coins we need for each amount.
+  dp[0] = 0; // To make 0, we need 0 coins.
+  for (let coin of coins) { // Check each coin
+    for (let i = coin; i <= amount; i++) { // Iterate through the entire amount from coin
+      dp[i] = Math.min(dp[i], dp[i - coin] + 1); // Update minimum number of needed coins.
+    }
+  }
+  return dp[amount] === Infinity ? -1 : dp[amount]; // If the last element is Infinity, then we cannot make the amount.
+};
+
+
+console.log(coinChange([1,2,5],11));
+// Output: 3
+
+// Solution 2
 const coinChangeProblem = (coins, amount) => {
     const coinsLength = coins.length;
     const ways = Array(amount + 1).fill(0);
