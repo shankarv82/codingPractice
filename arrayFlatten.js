066e2b026ed7f203e2f03c3f5a5504fb78f77f17
shankarv82 <shankarv82@gmail.com>
2024-01-19 00:14:59 +0530

    Update and rename 10.arrayFlatten.js to arrayFlatten.js

--- arrayFlatten.js
+++ arrayFlatten.js
@@ -1,12 +1,6 @@
 // Flattening an array. Ex: Input : [[1, 2, 3], 4, [5, [6, 7]] Output: [1, 2, 3, 4, 5, 6, 7].
 
-const flatten = arr => {
-  return arr.reduce((acc, current) => {
-    return acc.concat(Array.isArray(current) ? flatten(current) : current);
-  }, []);
-}
-
-
+const flatten = arr => arr.reduce((acc, current) => acc.concat(Array.isArray(current) ? flatten(current) : current), []);
 
 console.log(flatten([[1, 2, 3], 4, [5, [6, 7]]])); 
 
