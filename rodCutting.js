9d083b4d9dfa8ea84f9f889984d0af5a8e85c6e3
shankarv82 <shankarv82@gmail.com>
2024-01-18 23:09:28 +0530

    Update and rename 7_rodCutting.js to rodCutting.js

--- rodCutting.js
+++ rodCutting.js
@@ -1,16 +1,16 @@
 const cutRod = (prices, n) => {
-    const val = [0];
+    const dp = [0];
 
     for (let i = 1; i <= n; i++) {
         let maximum = Number.MIN_VALUE;
         for (let j = 0; j < i; j++) {
-            maximum = Math.max(maximum, prices[j] + val[i - j - 1]);
+            maximum = Math.max(maximum, prices[j] + dp[i - j - 1]);
         }
 
-        val[i] = maximum;
+        dp[i] = maximum;
     }
 
-    return val[n];
+    return dp[n];
 };
 
 const arr = [1, 5, 8, 9, 10, 17, 17, 20];
