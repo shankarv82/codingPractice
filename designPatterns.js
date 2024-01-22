d03280b15b34f8ba03136094e08284eece4e298f
shankarv82 <shankarv82@gmail.com>
2024-01-22 17:34:13 +0530

    Create designPatterns.js

---
+++ designPatterns.js
@@ -0,0 +1,19 @@
+What Are Design Patterns?
+  
+Creational Design Patterns
+  Singleton Pattern - One immutabel instance
+  Factory Method Pattern - Updatable obj after creation
+  Abstract Factory Pattern - Hide object creation logic
+  Builder Pattern - Build object step by step
+  Prototype Pattern - Borrow methods and attributes from other object
+
+Structural Design Patterns
+  Adapter Pattern - Parse data to required format
+  Decorator Pattern - Context provider (attach behavior) Share data across multiple components
+  Facade Pattern - simplified interface - map, for, reduce, sort
+  Proxy Pattern - substitute/placeholder for other object - error handling middleware, before calling API  
+
+Behavioral Design Patterns
+  Chain of Responsibility Pattern
+  Iterator Pattern
+  Observer Pattern - useEffect, eventlistners
