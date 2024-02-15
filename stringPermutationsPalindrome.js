f1a09a7b664eb410e02380e8abb576768938960d
shankarv82 <shankarv82@gmail.com>
2024-02-15 21:23:55 +0530

    Update stringPermutationsPalindrome.js

--- stringPermutationsPalindrome.js
+++ stringPermutationsPalindrome.js
@@ -9,6 +9,21 @@ const checkPalindrom = str => {
     return str === reverse(str);
 }
 
+const results = [];
+// Method 1
+const getAllPermutations2 = (string, prefix) => {
+  if (string.length === 0) {
+    results.push(prefix);
+  } else {
+    for (let i= 0; i < string.length; i++) {
+      const charsLeft = string.substring(0, i) + string.substring(i + 1);
+      getAllPermutations2(charsLeft, prefix + string[i]);
+    }
+  }
+  return results;
+}
+
+// Method 2
 const getAllPermutations = string => {
   const results = [];
 
@@ -28,7 +43,7 @@ const getAllPermutations = string => {
   return results;
 }
 
-const permutaions = getAllPermutations('abc');
+const permutaions = getAllPermutations2('abc', ''); // getAllPermutations('abc');
 
 for(let str  of permutaions) {
   console.log(`Is ${str} Palindrome? `, checkPalindrom(str));
