3d3cfe4cf2aa72ca76068233637dafd2f623a62b
shankarv82 <shankarv82@gmail.com>
2024-03-10 11:47:19 +0530

    Update and rename 6_minCoinsChange.js to minCoinsChange.js

--- minCoinsChange.js
+++ minCoinsChange.js
@@ -1,3 +1,23 @@
+const minCoins2 = (coins, amount) => {
+    if (amount <= 0) {
+        return 0;
+    }
+
+    const dp = new Array(amount + 1).fill(amount + 1);
+    dp[0] = 0;
+
+    for (let a=1; a<=amount; a++) {
+        for (let coin of coins) {
+            if(a-c >= 0) {
+                dp[a] = Math.min(dp[a], 1 + dp[a-c])
+            }
+        }
+    }
+
+    return dp[amount] === amount + 1 ? return -1 : return dp[amount];
+}
+
+
 const minCoins = (coins, V) => {
     let m = coins.length;
     let table = [0];
